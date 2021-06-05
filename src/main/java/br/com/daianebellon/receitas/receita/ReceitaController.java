2cc0efcef78e2b459b30ee218a4564aafce44bcf
daiane.bellon <daianebellon@hotmail.com>
2021-06-05 01:41:16 -0300

    Criando um service

--- src/main/java/br/com/daianebellon/receitas/receita/ReceitaController.java
+++ src/main/java/br/com/daianebellon/receitas/receita/ReceitaController.java
@@ -3,37 +3,32 @@ package br.com.daianebellon.receitas.receita;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.NoResultException;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/receita")
 public class ReceitaController {
 
     @Autowired
-    private ReceitaRepository receitaRepository;
+    private ReceitaService receitaService;
 
     @GetMapping("/")
     public List<ReceitaEntity> buscarReceitas() {
-        return receitaRepository.findAll();
+        return receitaService.buscarReceitas();
     }
 
     @GetMapping("/{id}")
     public ReceitaEntity buscarPeloId(@PathVariable Long id) {
-        Optional<ReceitaEntity> receita = receitaRepository.findById(id);
-        return receita.orElseThrow(() -> new NoResultException("Não encontrou!"));
+        return receitaService.buscarPeloId(id);
     }
 
     @DeleteMapping("/{id}")
     public void deletarPeloId(@PathVariable Long id) {
-        receitaRepository.deleteById(id);
+        receitaService.deletarPeloId(id);
     }
 
     @PostMapping("/")
     public void cadastrar(@RequestBody ReceitaEntity receitaEntity) {
-        System.out.println(receitaEntity);
-
-        receitaRepository.save(receitaEntity);
+        receitaService.cadastrar(receitaEntity);
     }
 }
---
+++ src/main/java/br/com/daianebellon/receitas/receita/ReceitaService.java
@@ -0,0 +1,38 @@
+package br.com.daianebellon.receitas.receita;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.NoResultException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReceitaService {
+
+    @Autowired
+    private ReceitaRepository receitaRepository;
+
+    @Transactional(readOnly = true)
+    public List<ReceitaEntity> buscarReceitas() {
+        return receitaRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public ReceitaEntity buscarPeloId(Long id) {
+        Optional<ReceitaEntity> receita = receitaRepository.findById(id);
+        return receita.orElseThrow(() -> new NoResultException("Não encontrou!"));
+    }
+
+    @Transactional
+    public void deletarPeloId(Long id) {
+        receitaRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void cadastrar(ReceitaEntity receitaEntity) {
+        System.out.println(receitaEntity);
+        receitaRepository.save(receitaEntity);
+    }
+}
